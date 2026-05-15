a2661d96cd2787d222eaf1e1c27048bf22eddd0a
Michele M S <semcafe.dev@gmail.com>
2026-05-15 09:04:45 -0300

    enviando as atualizações no html, foi criado o cad financeiro

--- js/abreMenus.js
+++ js/abreMenus.js
@@ -5,6 +5,7 @@
     if (sub){
         sub.classList.toggle('show');
     }
+}
 
     function carregar(pagina, event) {
         // 1. Atualiza visual do menu lateral
@@ -21,23 +22,23 @@
         // 3. Muda a página dentro do iframe
         const frame = document.getElementById('frame-conteudo');
         if (!frame) return;
-        
+
         switch(pagina) {
-            case 'cliente': frame.src = '/cliente/cadastro'; break;
-            case 'produtos': frame.src= '/produtos/cadastro'; break;
-            case 'financeiro': frame.src = '/financeiro/lancamento'; break;
-            case 'lojas': frame.src = '/lojas/cadastro'; break;
-            case 'marca': frame.src = '/marca/cadastro'; break;
-            case 'cor': frame.src = '/cores/cadastro'; break;
-            case 'tamanho': frame.src = '/tamanho/cadastro'; break;
-            case 'cartoes': frame.src = '/cartao/cadastro'; break;
+            case 'cliente': frame.src = 'paginas/CadCliente.html'; break;
+            case 'produtos': frame.src= 'paginas/CadProduto.html'; break;
+            case 'financeiro': frame.src = 'paginas/CadFinanceiro.html'; break;
+            case 'lojas': frame.src = 'paginas/CadLojas.html'; break;
+            case 'marca': frame.src = 'paginas/CadMarca.html'; break;
+            case 'cor': frame.src = 'paginas/CadCores.html'; break;
+            case 'tamanho': frame.src = 'paginas/CadTamanho.html'; break;
+            case 'cartoes': frame.src = 'paginas/CadCartao.html'; break;
             // Exemplo para os relatórios
-            case 'relatorio1': frame.src = '/relatorios/Compras'; break;
-            case 'relatorio2': frame.src = '/relatorios/Debitos'; break;
-            case 'relatorio3': frame.src = '/relatorios/LimiteCartao'; break;
+            case 'relatorio1': frame.src = 'paginas/Relatorio1.html'; break;
+            case 'relatorio2': frame.src = 'paginas/Relatorio2.html'; break;
+            case 'relatorio3': frame.src = 'paginas/Relatorio3.html'; break;
             default:
                 console.log("Página não encontrada: " + pagina);
         }
     }
     
-    }
+    
